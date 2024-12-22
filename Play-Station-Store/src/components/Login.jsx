88b3d8eab7beab01ea4b4e2ede03fdbcac21c74e
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2024-12-22 22:09:08 +0530

    Registering User account using Email and password with Firebase authentication

--- Play-Station-Store/src/components/Login.jsx
+++ Play-Station-Store/src/components/Login.jsx
@@ -1,9 +1,33 @@
 import HeaderLogo from "../assets/images/HeaderLogo.png";
-import React from "react";
+import React, { useState } from "react";
 import "../assets/styles/Login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { db, auth, registerUser } from "../firebase";
 
 function Login() {
+  const navigate = useNavigate();
+
+  const [email, setnewEmail] = useState("");
+  const [password, setnewPassword] = useState("");
+
+  const signIn = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    console.log(auth);
+  };
+
+  const register = (e) => {
+    e.preventDefault();
+    registerUser(email, password)
+      .then((user) => {
+        // Executes when succesfully created user with email and password
+        if (user) {
+          navigate("/");
+        }
+      })
+      .catch((error) => alert(error.message));
+  };
+
   return (
     <>
       <div className="login">
@@ -15,23 +39,35 @@ function Login() {
           </Link>
 
           <form className="loginForm">
-            <label className="loginFormLabel">
-              Username:
-              <input type="text" name="name" />
+            <label className="loginFormLabel label1">
+              Email :
+              <input
+                type="text"
+                name="name"
+                value={email}
+                onChange={(e) => setnewEmail(e.target.value)}
+              />
             </label>
-            <label className="loginFormLabel">
-              Password:
-              <input type="password" name="password" />
+            <label className="loginFormLabel label2">
+              Password :
+              <input
+                type="password"
+                name="password"
+                vlaue={password}
+                onChange={(e) => setnewPassword(e.target.value)}
+              />
             </label>
             <Link to="/forgot-password">
               <p>Forgot your password?</p>
             </Link>
-            <button type="submit">Sign in</button>
+            <button type="submit" onClick={signIn}>
+              Sign in
+            </button>
 
             <div className="loginFormRegister">
               <p>Don't have an account?</p>
               <Link to="/register">
-                <button>Register</button>
+                <button onClick={register}>Register</button>
               </Link>
             </div>
           </form>
---
+++ Play-Station-Store/src/firebase.js
@@ -0,0 +1,39 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore"; // For Firestore 
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDLwvhOZMIJYfPqo1W7VquWgdNrTQAXbr8",
+    authDomain: "play-station-store-e6933.firebaseapp.com",
+    projectId: "play-station-store-e6933",
+    storageBucket: "play-station-store-e6933.firebasestorage.app",
+    messagingSenderId: "394555522070",
+    appId: "1:394555522070:web:0db9216aed401a78b7c937",
+    measurementId: "G-R83W8ELHNX"
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const db = getFirestore(firebaseApp)
+const auth = getAuth(firebaseApp);
+
+const registerUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // User registered successfully 
+            const user = userCredential.user;
+            console.log("User registered:", user);
+            return user;
+        })
+        .catch((error) => { // Handle errors here 
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.error("Error registering user:", errorCode, errorMessage);
+            throw error;
+        });
+};
+
+export { db, auth, registerUser };
