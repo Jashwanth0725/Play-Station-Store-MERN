d3781d1549f7933e12f5ef1ca5116081879d39ca
Tandamalla Jashwanth Sai <jashwanthsai07251@gmail.com>
2024-12-29 15:17:33 +0530

    Server Setup done

---
+++ Play-Station-Store/Server/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+});
+
+
+//get jokes
+app.get('/jokes', (req, res) => {
+    const jokes = [
+        {
+            joke: "joke1"
+        },
+        {
+            joke: "joke 2"
+        },
+        {
+            joke:
+                "joke 3"
+        }];
+    res.send(jokes);
+})
+
+
+
+const port = process.env.PORT || 5000;
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+});
